5f9a4d4a03c351d99ad50323b609ac8d5829c692
theBGuy <60308670+theBGuy@users.noreply.github.com>
2024-02-11 02:24:34 -0500

    debugging

--- build/updateConfig.cjs
+++ build/updateConfig.cjs
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const file = require("../wails.json");
 
+console.log(process.argv);
 const vString = process.argv[2];
 if (!vString || !vString.startsWith("v") || vString.split(".").length !== 3) {
   throw new Error("No version or invalid format. Please use v0.0.0 format.", vString);
@@ -9,11 +10,10 @@ if (!vString || !vString.startsWith("v") || vString.split(".").length !== 3) {
 console.log(`changing version to ${vString} and environment to production`)
 
 const versionNum = `${vString}`.replace("v", "")
-file.version = versionNum;
 file.info.productVersion = versionNum;
 file.info.environment = "production";
 
-fs.writeFile("wails.json", JSON.stringify(file), function writeJSON(err) {
+fs.writeFile("wails.json", JSON.stringify(file, null, 2), function writeJSON(err) {
   if (err) return console.log(err);
   console.log(JSON.stringify(file, null, 2));
 });
