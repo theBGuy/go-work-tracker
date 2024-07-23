d0d298bec45c1b54503837318d1f9436946edd66
theBGuy <60308670+theBGuy@users.noreply.github.com>
2024-07-23 15:22:30 -0400

    Create NavBar.tsx

---
+++ frontend/src/components/NavBar.tsx
@@ -0,0 +1,38 @@
+import { NavLink } from 'react-router-dom';
+import { Typography, Box, Stack } from '@mui/material';
+
+const NavBar = () => {
+  const linkStyle = {
+    textDecoration: 'none',
+    color: 'white',
+  };
+
+  const activeLinkStyle = {
+    textDecoration: 'underline',
+    color: 'yellow',
+  };
+
+  return (
+    // <Box sx={{ display: 'flex', alignItems: 'center', backgroundColor: '#333', padding: '10px' }}>
+    <Stack direction="row" spacing={2} justifyContent="center" alignItems="center">
+      <NavLink to="/" style={({ isActive }) => (isActive ? activeLinkStyle : linkStyle)}>
+        <Typography variant="h6" component="h1" sx={{ flexGrow: 1 }}>
+          Home
+        </Typography>
+      </NavLink>
+      <NavLink to="/table" style={({ isActive }) => (isActive ? activeLinkStyle : linkStyle)}>
+        <Typography variant="h6" component="h1" sx={{ flexGrow: 1 }}>
+          Table
+        </Typography>
+      </NavLink>
+      <NavLink to="/charts" style={({ isActive }) => (isActive ? activeLinkStyle : linkStyle)}>
+        <Typography variant="h6" component="h1" sx={{ flexGrow: 1 }}>
+          Charts
+        </Typography>
+      </NavLink>
+    </Stack>
+    // </Box>
+  );
+};
+
+export default NavBar;
