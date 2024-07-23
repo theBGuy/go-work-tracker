3b2b2f859e5ef7c5ca6981c3abd3d5daa7b4f74f
theBGuy <60308670+theBGuy@users.noreply.github.com>
2024-07-23 19:23:25 -0400

    Initialize `Charts` page with `LineChart` from `mui`

--- frontend/src/routes/Charts.tsx
+++ frontend/src/routes/Charts.tsx
@@ -1,8 +1,81 @@
-import { AppBar, Toolbar, Typography } from "@mui/material"
-import { NavLink } from "react-router-dom"
-import NavBar from "../components/NavBar"
+import {
+  AppBar,
+  Box,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Paper,
+  Select,
+  Stack,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import NavBar from "../components/NavBar";
+import { useEffect, useState } from "react";
+import { getMonth, months } from "../utils/utils";
+import { useStore } from "../stores/main";
+import { main } from "../../wailsjs/go/models";
+import { GetDailyWorkTimeByMonth, GetProjects } from "../../wailsjs/go/main/App";
+import { LineChart } from "@mui/x-charts/LineChart";
 
-function Charts () {
+interface GraphData {
+  // @ts-ignore
+  day: string;
+  [project: string]: number;
+}
+
+function Charts() {
+  const currentYear = new Date().getFullYear();
+  const years = Array.from({ length: currentYear - 1999 }, (_, i) => 2000 + i);
+  const organizations = useStore((state) => state.organizations);
+  const [selectedOrganization, setSelectedOrganization] = useState("");
+  const [projects, setProjects] = useState<main.Project[]>([]);
+  const [dailyWorkTimes, setDailyWorkTimes] = useState<GraphData[]>([]);
+  const [selectedYear, setSelectedYear] = useState(currentYear);
+  const [selectedMonth, setSelectedMonth] = useState(getMonth());
+  const colors = [
+    "lightgray",
+    "lightgreen",
+    "lightblue",
+    "lightcoral",
+    "lightcyan",
+    "lightgoldenrodyellow",
+    "blue",
+    "red",
+    "orange",
+    "yellow",
+    "black",
+  ];
+
+  useEffect(() => {
+    const store = useStore.getState();
+    // deep clone to avoid mutating the original objects we just want the initial states
+    setProjects(store.getProjects());
+    setSelectedOrganization(store.getSelectedOrganization());
+  }, []);
+
+  useEffect(() => {
+    if (!selectedOrganization) return;
+    GetProjects(selectedOrganization).then((projs) => {
+      setProjects(projs);
+    });
+  }, [selectedOrganization]);
+
+  useEffect(() => {
+    if (!selectedOrganization) return;
+    GetDailyWorkTimeByMonth(selectedYear, selectedMonth, selectedOrganization).then((data) => {
+      console.log("daily work times", data);
+      const graphData: GraphData[] = [];
+      for (const [day, projs] of Object.entries(data)) {
+        console.log("day", new Date(day).getDate());
+        // @ts-ignore
+        graphData.push({ day: new Date(day), ...projs });
+      }
+      console.log("graph data", graphData);
+      setDailyWorkTimes(graphData);
+    });
+  }, [selectedYear, selectedOrganization, selectedMonth]);
+
   return (
     <div id="app">
       <AppBar position="static">
@@ -10,8 +83,83 @@ function Charts () {
           <NavBar />
         </Toolbar>
       </AppBar>
+      <Stack spacing={1} sx={{ padding: 2 }}>
+        <Paper sx={{ padding: 2 }}>
+          <Stack direction="row" spacing={2}>
+            <FormControl fullWidth margin="normal">
+              <InputLabel>Year</InputLabel>
+              <Select
+                variant="filled"
+                value={selectedYear}
+                onChange={(event) => setSelectedYear(event.target.value as number)}
+              >
+                {years.map((year) => (
+                  <MenuItem key={year} value={year}>
+                    {year}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <FormControl fullWidth margin="normal">
+              <InputLabel>Month</InputLabel>
+              <Select value={selectedMonth} onChange={(event) => setSelectedMonth(event.target.value as number)}>
+                {Object.entries(months).map(([monthNumber, monthName]) => (
+                  <MenuItem key={Number(monthNumber)} value={Number(monthNumber)}>
+                    {monthName}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <FormControl fullWidth margin="normal">
+              <InputLabel>Organization</InputLabel>
+              <Select
+                value={selectedOrganization}
+                onChange={(event) => {
+                  const foundOrg = organizations.find((org) => org.name === event.target.value);
+                  if (foundOrg) {
+                    setSelectedOrganization(foundOrg.name);
+                  }
+                }}
+              >
+                {organizations.map((org) => (
+                  <MenuItem key={org.id} value={org.name}>
+                    {org.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Stack>
+        </Paper>
+        {/* Fix setting the height manually */}
+        <Paper elevation={1} sx={{ height: 400 }}>
+          <LineChart
+            xAxis={[
+              {
+                min: new Date(selectedYear, selectedMonth - 1, 1),
+                max: new Date(selectedYear, selectedMonth, 0),
+                dataKey: "day",
+                label: "Date",
+                scaleType: "utc",
+              },
+            ]}
+            yAxis={[
+              {
+                label: "Total Work Time (s)",
+                scaleType: "linear",
+              },
+            ]}
+            series={projects.map((project, index) => ({
+              dataKey: project.name,
+              label: project.name,
+              color: colors[index],
+              stack: "total",
+            }))}
+            dataset={dailyWorkTimes}
+          />
+        </Paper>
+      </Stack>
     </div>
-  )
+  );
 }
 
-export default Charts
+export default Charts;
