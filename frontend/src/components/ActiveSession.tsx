14e03ec40b5f1103a63ef71518fadca3e625a4b2
theBGuy <60308670+theBGuy@users.noreply.github.com>
2024-07-26 15:00:38 -0400

    Update ActiveSession.tsx
    - Add organization and project name to mini-timer

--- frontend/src/components/ActiveSession.tsx
+++ frontend/src/components/ActiveSession.tsx
@@ -1,8 +1,9 @@
-import { Card, CardContent, CardHeader, Typography, CardActions, Button, Box, IconButton } from "@mui/material";
+import { Card, CardContent, CardHeader, Typography, CardActions, Button, Box, IconButton, Stack } from "@mui/material";
 import { formatTime } from "../utils/utils";
 import { useTimerStore } from "../stores/timer";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import StopIcon from "@mui/icons-material/Stop";
+import { useAppStore } from "../stores/main";
 
 interface activeSessionProps {
   stopTimer: () => void;
@@ -13,6 +14,8 @@ const ActiveSession: React.FC<activeSessionProps> = ({ stopTimer, mode = "normal
   const startTimer = useTimerStore((state) => state.startTimer);
   const elapsedTime = useTimerStore((state) => state.elapsedTime);
   const timerRunning = useTimerStore((state) => state.running);
+  const org = useAppStore((state) => state.selectedOrganization);
+  const proj = useAppStore((state) => state.selectedProject);
 
   return (
     <>
@@ -43,22 +46,24 @@ const ActiveSession: React.FC<activeSessionProps> = ({ stopTimer, mode = "normal
             position: "fixed",
             bottom: 70,
             left: 5,
-            alignItems: "center",
-            padding: 2,
+            padding: 1.5,
             boxShadow: 3,
-            borderRadius: 12,
+            borderRadius: 4,
             zIndex: 1000,
-            display: "flex",
-            flexDirection: "row",
             backgroundColor: "rgba(0, 0, 0, 0.5)"
           }}
           >
-            <Typography variant="body1">
-              {timerRunning ? formatTime(elapsedTime) : "00h 00m 00s"}
-            </Typography>
-            <IconButton color="inherit" size="small" onClick={timerRunning ? stopTimer : startTimer}>
-              {timerRunning ? <StopIcon /> : <PlayArrowIcon />}
-            </IconButton>
+            <Stack>
+              <Typography variant="caption">{org} - {proj}</Typography>
+              <Stack direction="row" alignItems="center" flex={1} justifyContent="space-between">
+                <Typography variant="body1">
+                  {timerRunning ? formatTime(elapsedTime) : "00h 00m 00s"}
+                </Typography>
+                <IconButton color="inherit" size="small" onClick={timerRunning ? stopTimer : startTimer}>
+                  {timerRunning ? <StopIcon /> : <PlayArrowIcon />}
+                </IconButton>
+              </Stack>
+            </Stack>
           </Box>
         )}
     </>
