36f18ecc10723f0e23ada6e17c224a9cb313c66e
theBGuy <60308670+theBGuy@users.noreply.github.com>
2024-10-21 00:22:49 -0400

    Add fontsource dependency for @mui

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,7 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import { GetAllProjects, GetWorkSessions, ShowWindow, TimeElapsed } from "@go/main/App";
 import CssBaseline from "@mui/material/CssBaseline";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
